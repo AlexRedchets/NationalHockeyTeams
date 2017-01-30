5ca97fa08d9a7b3e3dd5f0db13b9f09e312c7268
AlexRedchets <alexey.redchets@gmail.com>
2017-01-30 15:03:13 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var port = 8080;
+
+var app = express();
+
+//Connect to mongoDB
+mongoose.connect('mongodb://localhost/nht');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+require('./routes/routes')(app); // load routes and pass in app
+
+app.listen(port);
+console.log('server is listening on port ' + port);
+
+module.exports = app;
